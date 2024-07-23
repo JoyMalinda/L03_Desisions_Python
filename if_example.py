978f6b0ef797e8e38750d3f4ebe13ae1290aa917
koki <kokimalinda@gmail.com>
2024-07-23 20:14:56 +0300

    first commit

---
+++ if_example.py
@@ -0,0 +1,18 @@
+#def if_else_example():
+    #age=15
+    #if(age>=18):
+       #print("Eligible for Voting")
+    #else:
+        #print("Not Eligible for Voting")
+#if_else_example()
+
+def break_example():
+    y=5
+    for i in range(0,y+1):
+        if i == y:
+            print("Thank you!")
+            break
+        else:
+            print(i)
+    print("End of Prg")        
+break_example()
